3277bb92016e5627615545976c92c0db25b483ad
Imane BOUROUYNE (MA/FR) <110111059+Curiosity10001@users.noreply.github.com>
2022-08-29 12:33:25 +0200

    animation activation with movement
    Jump still under comprehension/work

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,10 +7,8 @@ public class PlayerMovement : MonoBehaviour
     public float _runVelocity = 1.2f;
     public float _fallSpeed = 2f;
     public float _jumpForce = 1.5f;
-    public int _maxJump = 3;
+    public int _maxJump = 2;
 
-    
-    private bool _jump = false;
     private int _jumpNB = 0;
 
     [Header("Other impacted Gameobject in script")]
@@ -18,19 +16,58 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D _rdbdDwarves;
     public Animator _animator;
 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal")*_runVelocity;
-        float y = Input.GetAxis("Vertical") * _jumpForce;
-        _rdbdDwarves.velocity= new Vector2(x,y);
+        float x = Input.GetAxisRaw("Horizontal") ;
+        float y = Input.GetAxisRaw("Vertical");
+        _rdbdDwarves.velocity = new Vector2(x * _runVelocity, y * _jumpForce);
+
+        if ((x == 0 && y == 0))
+        {
+            _animator.SetBool("isIdle", true);
+        }
+        else 
+        { 
+            _animator.SetBool("isIdle", false); 
+        }
+        ;
+        if (y > 0)
+        {
+            _animator.SetBool("isJumping", true);
+        }
+        else
+        {
+            _animator.SetBool("isJumping", false);
+        };
+        if (y < 0)
+        {
+            _animator.SetBool("isFalling", true);
+        }
+        else
+        {
+            _animator.SetBool("isFalling", false);
+        }
+        if (x < 0 || x > 0)
+        {
+            _animator.SetBool("isRunning", true);
+        }
+        else 
+        {
+            _animator.SetBool("isRunning", false);
+        }
+    }
+    private void FixedUpdate()
+    {
         
-        if () { };
     }
 }
+   
